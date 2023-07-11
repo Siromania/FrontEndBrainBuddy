7d396c10507b1d04fd25ed072d480faa2aa13346
Fombasso Theo <theo.fombasso@gmail.com>
2023-07-11 16:28:36 +0200

    unfinish

--- src/app/components/monkey-test-block/monkey-test-block.component.ts
+++ src/app/components/monkey-test-block/monkey-test-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-monkey-test-block',
@@ -6,8 +6,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./monkey-test-block.component.scss']
 })
 export class MonkeyTestBlockComponent {
-  number : string = "1";
+  numberOrder : string = "1";
   @Input() x : number | undefined;
   @Input() y : number | undefined;
+  @Output() onSelectValue = new EventEmitter<{xValue: any , yValue: any}>();
+
+  // onClickEmit() {
+  //   this.onSelectValue.emit( {xValue: this.x, yValue:this.y} );
+  // }
+
+  @HostListener('click', ['$event.target']) onClick() {
+    console.log("x="+this.x+" y="+this.y);
+    this.onSelectValue.emit( {xValue: this.x, yValue:this.y} );
+  }
 
 }
--- src/app/components/monkey-test-grid/monkey-test-grid.component.ts
+++ src/app/components/monkey-test-grid/monkey-test-grid.component.ts
@@ -7,24 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonkeyTestGridComponent implements OnInit {
 
-  maxSize : number = 10;
+  maxSize : number = 9;
   round : number = 5;
   listRow : number[] = [1,2,3,4,5,6,7,8,9];
   listColumn : number[] = [1,2,3,4,5,6,7,8,9];
-  
+  rowX : any = 0;
+  columnY : any = 0;
+  listRandom : any = [];
+  currentRound : number = 0;
+
   ngOnInit(): void {
-   
     console.log(this.random());
-
-    for(let i = 0; i<this.round;++i){
-      console.log("x="+this.random()+" y="+this.random());
-
+    for(let i = 1; i<this.round;++i){
+      let myVar : any[]= [{ 
+          "x": this.random(),
+          "y": this.random()
+      }]
+      this.listRandom.push(myVar);
     }
+    console.log(this.listRandom);
 
   }
 
   random(){
-    return Math.floor(Math.random() * this.maxSize);
+    return Math.floor(Math.random() * this.maxSize+1);
+  }
+
+  selectValue( newValue : any ) {
+    console.log(newValue);
+    console.log(this.listRandom[0][0]);
+    for(let i = 0;i<this.listRandom.length;++i){
+      if(newValue.xValue === this.listRandom[i][0].x && newValue.yValue === this.listRandom[i][0].y){
+        console.log("right");
+      }
+    }
   }
 
 
