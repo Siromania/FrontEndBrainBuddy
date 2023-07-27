aa079f61dcd9108cb49d2c4f35489652a2bdbfc3
Fombasso Theo <theo.fombasso@gmail.com>
2023-07-27 10:31:51 +0200

    innit commit

---
+++ src/main/java/backEnd/BrainBuddySpring/BrainBuddySpringApplication.java
@@ -0,0 +1,13 @@
+package backEnd.BrainBuddySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBuddySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBuddySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backEnd/BrainBuddySpring/BrainBuddySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package backEnd.BrainBuddySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBuddySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
