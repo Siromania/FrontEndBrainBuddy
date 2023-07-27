70c7527dc93e5f32b88b83727fae85e51adc01b4
VAUBAN_C5\Administrateur <k.aberbach@hotmail.com>
2023-07-27 12:16:23 +0200

    Controller_users_post

--- src/main/java/backEnd/BrainBuddySpring/Controllers/UsersController.java
+++ src/main/java/backEnd/BrainBuddySpring/Controllers/UsersController.java
@@ -1,7 +1,12 @@
 package backEnd.BrainBuddySpring.Controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import backEnd.BrainBuddySpring.Entities.Users;
@@ -17,5 +22,22 @@ public class UsersController {
     public Iterable<Users> getUsers() {
         Iterable<Users> list = this.userRepo.findAll();
         return list;
+    }
+
+    @GetMapping("/users/{id}")
+    public Users getUser(@PathVariable Integer id) {
+
+        Optional<Users> optionalUser = this.userRepo.findById(id);
+        if(!optionalUser.isPresent()) {
+            return null;
+        }
+
+        Users userToReturn = optionalUser.get();
+        return userToReturn;
+    }
+
+    @PostMapping("/users")
+    public Users createUser(@RequestBody Users user) {
+        return this.userRepo.save(user);
     }
 }
